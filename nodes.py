d8997587d79f68d624ac849ca276ea542e3cc093
fractalbach <fractalbach@gmail.com>
2018-11-17 17:54:33 -0800

    added a method of randomly creating connections between nodes on the same level

--- nodes.py
+++ nodes.py
@@ -5,16 +5,19 @@ import random
 class Node:
     def __init__(self, level):
         self.level = level
-        self.connections = []
+        self.connections = set()
 
     def __repr__(self):
         return "(L{0},d{1})".format(self.level, self.degree())
+
+    def details(self):
+        return "{1}".format(self.level, self.connections)
 
     def degree(self):
         return len(self.connections)
     
     def connect(self, LocationTuple):
-        self.connections += [LocationTuple]
+        self.connections.add(LocationTuple)
 
         
         
@@ -24,15 +27,19 @@ class TreeTable:
 
     def __repr__(self):
         s = ""
-        for row in self.table:
-            s += repr(row) + '\n'
+        for i, row in enumerate(self.table):
+            for j, node in enumerate(row):
+                s+= "{0}{1}: {2}\n".format(i*'\t', j, node.details())
         return s
 
     def info(self):
         print('Table Info:')
         for i, v in enumerate(self.table):
             print('\tlevel {0} : number of nodes = {1}'.format(i, len(v)))
-        
+
+    def depth(self):
+        return len(self.table)
+    
     def getFirstNode(self):
         return self.table[0][0]
 
@@ -47,8 +54,36 @@ class TreeTable:
     def connect(self, Loc1, Loc2):
         n1 = self.getNode(Loc1)
         n2 = self.getNode(Loc2)
-        n1.connect(Loc1)
-        n2.connect(Loc2)
+        n1.connect(Loc2)
+        n2.connect(Loc1)
+
+    def createSinglePath(self):
+        prev = (0, 0)
+        for i in range(1, len(self.table)):
+            nNodes = len(self.table[i]) - 1
+            j = random.randint(0, nNodes)
+            current = (i, j)
+            self.connect(prev, current)
+            prev = current
+
+    def randConnectsThisLevel(self, node1, nConnects):
+        """make random connections between nodes on the same level."""
+        row, col = node1
+        nNodes = len(self.table[row]) - 1
+        for _ in range(nConnects):
+            node2 = (row, random.randint(0, nNodes))
+            if node1 != node2:
+                self.connect(node1, node2)
+
+    def createSinglePathWithDeadEnds(self, nConnects):
+        prev = (0, 0)
+        for i in range(1, len(self.table)):
+            nNodes = len(self.table[i]) - 1
+            current = (i, random.randint(0, nNodes))
+            self.connect(prev, current)
+            self.randConnectsThisLevel(current, nConnects)
+            prev = current
+    
         
     
 def makeTable(levels, minNodesPerLevel, maxNodesPerLevel):
@@ -71,7 +106,7 @@ def makeTable(levels, minNodesPerLevel, maxNodesPerLevel):
 #  Main
 # ______________________________________________________________________
 
-levels = 3
+levels = 5
 minNodes = 3
 maxNodes = 9
 startDegree = 4
@@ -81,11 +116,6 @@ exitDegree = 4
 
 tree = TreeTable(levels, minNodes, maxNodes)
 tree.info()
-
-print("Table after initial creation:")
 print(tree)
-
-print("After adding some connections:")
-tree.connect([0,0], [1,1])
-tree.connect([0,0], [1,2])
+tree.createSinglePathWithDeadEnds(5)
 print(tree)
