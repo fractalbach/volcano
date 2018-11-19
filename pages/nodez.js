b5352e7272a35d4f2631a74f52ace2b944467776
fractalbach <fractalbach@gmail.com>
2018-11-18 17:02:18 -0800

    added a way to do addPath() by pressing a button, and see the random graph as it's generated

--- pages/nodez.js
+++ pages/nodez.js
@@ -1,13 +1,25 @@
-{
-
+var MyGraphCreator = (function() {
 
 let node = (id, level)=> {
 	return {'id':id, 'label':id, 'level':level};
 }
 
-// let edgeDataItem = (from, to)=> {
-// 	return {'from': from, 'to': to};
-// }
+let makeStartNode = ()=> {
+	let n = node('start', 0);
+	n['shape'] = 'star';
+	n['color'] = 'green';
+	return n;
+}
+let makeFinalNode = (level)=> {
+	let n = node('end', level);
+	n['shape'] = 'star';
+	n['color'] = 'orange';
+	return n;
+}
+
+let edgeDataItem = (from, to)=> {
+	return {'from': from, 'to': to};
+}
 
 let randint = (min, max)=> {
   	min = Math.ceil(min);
@@ -20,23 +32,40 @@ let makeTable = (nLevels, minNodes, maxNodes)=> {
 	// Create empty table.
 	let table = new Array(nLevels)
 	for (let i = 0; i < nLevels; i++) {
-		table[i] = new Array(maxNodes)
+		table[i] = new Array()
 	}
 
-	// Add first and last nodes to the table.
+	// define the last Index int.
 	let lastIndex = nLevels - 1
-	table[0][0] = node('start', 0)
-	table[lastIndex][0] = node('exit', lastIndex)
+
+	// Add first node to the table.
+	table[0].push(makeStartNode())
 
 	// Add a random number of nodes to each level in between.
 	for (let i = 1; i < lastIndex; i++) {
 		let nNodes = randint(minNodes, maxNodes);
 		for (let j = 0; j < nNodes; j++) {
-			table[i][j] = node(`room(${i},${j})`, i)
+			table[i].push(node(`room(${i},${j})`, i))
 		}
 	}
 
+	// Add the last node to the table.
+	table[lastIndex].push(makeFinalNode(lastIndex));
+
 	return table;
+}
+
+let createSinglePath = (table, chart)=> {
+	let prevRow = 0;
+	let prevCol = 0;
+	for (let i = 1; i < table.length; i++) {
+		let theChosenColumn = randint(0, table[i].length - 1);
+		let prevNode = table[prevRow][prevCol];
+		let currNode = table[i][theChosenColumn];
+		addEdge(chart, prevNode, currNode);
+		prevRow = i;
+		prevCol = theChosenColumn;
+	}
 }
 
 
@@ -44,7 +73,7 @@ let makeEdgeChart = (table)=> {
 	let chart = new Map();
 	for (let i = 0; i < table.length; i++) {
 		for (let j = 0; j < table[i].length; j++) {
-			if (!table[i][j]) {
+			if (typeof table[i][j] === 'undefined') {
 				continue
 			}
 			chart.set(table[i][j]['id'], new Set());
@@ -54,18 +83,55 @@ let makeEdgeChart = (table)=> {
 }
 
 let addEdge = (chart, node, target)=> {
-	if (chart.has(node)) {
-		chart.get(node).add(target);
+	if (chart.has(node.id)) {
+		chart.get(node.id).add(target.id);
 	}
 }
 
+/* 
+converts the list of nodes and edges into a structure that can
+be used for vis.js to display the graph.
+*/
+let combineGraphData = (nodeTable, edgeChart)=> {
+	let data = {
+		"nodes": [],
+		"edges": [],
+	}
+	for (let [i, row] of Object.entries(nodeTable)) {
+		for (let [j, node] of Object.entries(row)) {
+			if (typeof node !== 'undefined') {
+				data.nodes.push(node);
+			}
+		}
+	}
+	for (let [node, edgeSet] of edgeChart.entries()) {
+		if (edgeSet.size > 0) {
+			for (let nodeTarget of edgeSet.values()) {
+				data.edges.push(edgeDataItem(node, nodeTarget));
+			}
+		}
+	}
+	return data
+}
 
-let startGame = ()=> {
-	let nodeTable = makeTable(5, 3, 9);
+
+let initGraphCreator = ()=> {
+	let nodeTable = makeTable(8, 5, 10);
 	let edgeChart = makeEdgeChart(nodeTable);
-	console.log(nodeTable, edgeChart);
+	createSinglePath(nodeTable, edgeChart);
+	let dataForVis = combineGraphData(nodeTable, edgeChart);
+	let exportData = {nodeTable, edgeChart, dataForVis};
+	console.log(exportData);
+	return exportData;
 }
-startGame();
 
-
+let exportData = initGraphCreator();
+return {
+	dataForVis: exportData.dataForVis,
+	nodeTable: exportData.nodeTable,
+	edgeChart: exportData.edgeChart,
+	createSinglePath,
+	combineGraphData,
 }
+
+}());
