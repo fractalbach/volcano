e9ba81fd0ca36cdbffb962da0340d4e5c40fd712
fractalbach <fractalbach@gmail.com>
2018-11-19 22:52:54 -0800

    added toggle-able overlay buttons/tabs, and a way to add more.

--- pages/nodez.js
+++ pages/nodez.js
@@ -1,4 +1,4 @@
-var GraphMaster9000 = (function() {
+var GraphGenerator = (function() {
 
 	let node = (id, level)=> {
 		return {'id':id, 'label':id, 'level':level};
@@ -114,7 +114,7 @@ var GraphMaster9000 = (function() {
 	let _dataForVis;
 	let nPaths = 5;
 
-	class GraphMaster9000 {
+	class GraphGenerator {
 		constructor() {
 			this.nodeMatrix = makeTable(8, 5, 10);
 			this.edgeSetMap = makeEdgeChart(this.nodeMatrix);
@@ -131,13 +131,13 @@ var GraphMaster9000 = (function() {
 			return _dataForVis;
 		}
 	}
-	return new GraphMaster9000()
+	return new GraphGenerator()
 }());
 
 
 var gamestate = (function(){
 
-	let g = GraphMaster9000;
+	let g = GraphGenerator;
 	let _previousNode = undefined;
 	let _currentNode = 'start';
 	let _discoveredNodes = new Set(['start', 'end']);
@@ -166,7 +166,7 @@ var gamestate = (function(){
 
 
 
-var gamedisplay = (function() {
+var GraphDisplay = (function() {
 	let options = {
 		height: '100%',
   		width: '100%',
@@ -188,7 +188,7 @@ var gamedisplay = (function() {
 	    },
 	};
 
-	let g = GraphMaster9000;
+	let g = GraphGenerator;
 
 	let myNetwork = new vis.Network(
 		document.querySelector('#mynetwork'),
@@ -213,15 +213,85 @@ var gamedisplay = (function() {
 	updateGraph();
 	// changeView();
 
+	// _________________________________________
+	//  Overlay Togglers
+	// =========================================
 
-	let showFullMap = ()=> {
+	// Special Format: (button, overlay, originText, activeText)
+	// Used to create overlay togglers by pressing these buttons.
+	// This structure is exactly the same as the arguments for 
+	// the functoin MakeToggleFunc
+	let buttons_and_their_overlays = [
+		['#ButtonShowMenu', '#menuOverlay', 'show menu', 'hide menu'],
+	]
 
+	// currentlyEnabled corresponds to the index of buttons_and_their_overlays.
+	// when the value is -1, it means that there is no overlay active, and the
+	// default beneath will be shown.
+	let currentlyEnabled = -1;
+
+	let disable = (n)=> {
+		let [$button, $overlay, originText, activeText] = buttons_and_their_overlays[n];
+		let button = document.querySelector($button);
+		let overlay = document.querySelector($overlay);
+		overlay.classList.add('hidden');
+		button.classList.remove('ActivatedButton');
+		button.innerText = originText;
 	}
-	document.querySelector('#ButtonShowFullMap').addEventListener('click', showFullMap);
+
+	let enable = (n)=> {
+		let [$button, $overlay, originText, activeText] = buttons_and_their_overlays[n];
+		let button = document.querySelector($button);
+		let overlay = document.querySelector($overlay);
+		overlay.classList.remove('hidden');
+		button.classList.add('ActivatedButton');
+		button.innerText = activeText;
+	}
+
+	let updateAll = ()=> {
+		for (let n of buttons_and_their_overlays.keys()) {
+			if (n === currentlyEnabled) {
+				enable(n)
+			} else {
+				disable(n)
+			}
+		}
+	}
+
+	// initialize by making sure that the correct overlay is showing.
+	updateAll();
+
+
+	let toggleHandler = (n)=> {
+		if (n === currentlyEnabled) {
+			currentlyEnabled = -1;	
+		} else {
+			currentlyEnabled = n;
+		}
+		updateAll();
+	}
+
+	// MakeToggleFunc returns a function that can be used to toggle 
+	// the visibility of different overlays. Maintains the fact that
+	// only a single overlay should be visible at any given time.
+	let MakeToggleFunc = (n)=> {
+		return ()=> {
+			toggleHandler(n)
+		}
+	}
+
+	// generate the event listeners to toggle the windows.
+	for (let [n, entry] of buttons_and_their_overlays.entries()) {
+		let fn = MakeToggleFunc(n);
+		let buttonSel = entry[0];
+		document.querySelector(buttonSel).addEventListener('click', fn);
+		document.querySelector(buttonSel).addEventListener('keypress', fn);
+	}
+
 }());
 
 
 
 
-console.log(GraphMaster9000);
+console.log(GraphGenerator);
 console.log(gamestate);
