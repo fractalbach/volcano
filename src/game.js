ad804b3f844a4cc5699f4f20045b7006e29ecb92
fractalbach <fractalbach@gmail.com>
2018-11-26 21:59:54 -0800

    defined 'game', started entity definitions - loaded a new script: 'game.js' - created class Ent - myplayer is an instance of class Ent

--- src/game.js
+++ src/game.js
@@ -1,12 +1,43 @@
 var game = (function(){
 
-	let allNodes = new Set();
-	let discoveredNodes = new Set();
-	let solvedNodes = new Set();
-	let nodeToRoom = new Map();
+	const randint = (min, max)=> {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
-	let currentNode = {};
-	let previousNode = {};
+	const defaultStats = ()=> {
+		return {
+			health: 1000,
+			attack: 10,
+			defense: 10,
+			speed: 10,
+		}
+	}
 
+	class Ent {
+		constructor(stats) {
+			this.stats = defaultStats();
+			Object.assign(this.stats, stats);
+		}
+		rollAttack() {
+			return randint(0, this.attack);
+		}
+		rollDefend() {
+			return randint(0, this.defense);
+		}
+		rollDodge() {
+			return randint(0, this.speed);
+		}
+	}
+
+	let myplayer = new Ent();
+
+	return {
+		Ent,
+		myplayer,
+	}
 
 }());
+
+console.log('game:\n', game);
